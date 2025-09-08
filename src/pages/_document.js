fa052d5452c6ee8eac703f2b54750945121cfef1
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-09-08 11:37:07 +0000

    added google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,57 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    {/* Add Google Fonts for Montserrat */}
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-                        rel="stylesheet"
-                    />
-                    {/* Favicon Links */}
-                    <link rel="icon" href="/favicon.png" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    {/* Google Tag Manager Script */}
-
-                </Head>
-                <body>
-                {/* Google Tag Manager (noscript) */}
-
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Add Google Fonts for Montserrat */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          />
+          {/* Favicon Links */}
+          <link rel="icon" href="/favicon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8G01J8Y0HM"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-8G01J8Y0HM');
+                            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
